54c952255c5898081ff29ad7e0f662b5b437b19b
Waner Valencia <waner.valencia@antpack.co>
2021-11-08 16:52:55 -0500

    user profile without calendat

---
+++ src/styles/pages/ProfileStyles/index.js
@@ -0,0 +1,16 @@
+import { style } from "typestyle";
+import background from "../../../assets/bg_plataforma-8-1.jpg";
+const HomeStyle = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+  background: `url(${background})`,
+  backgroundSize: "cover",
+  color: "white",
+  $nest: {
+
+  },
+});
+
+export default HomeStyle;
