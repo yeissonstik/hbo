2f0939857e44acc146294e1af229029b0b143040
Waner Valencia <waner.valencia@antpack.co>
2021-11-09 12:28:33 -0500

    fix styles

--- src/components/UserCalendar/index.js
+++ src/components/UserCalendar/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
 
-import { ScheduleComponent, Day, Week, Inject, ViewsDirective, ViewDirective } from '@syncfusion/ej2-react-schedule';
-import { extend } from '@syncfusion/ej2-base';
-export default class UserCalendar extends React.Component {
-    constructor() {
-        super(...arguments);
-        this.data = extend([], [], null, true);
-    }
-    render() {
-        return <ScheduleComponent renderCell={()=><div>hola</div>} timeScale={{ enable: false }} width='100%' height='350px' selectedDate={new Date(2018, 1, 15)}>
-  <ViewsDirective>
-    <ViewDirective option='Week' interval={1} displayName='1 Weeks' showWeekend={true} isSelected={true}/>
-  </ViewsDirective>
-  <Inject services={[Week]}/>
-</ScheduleComponent>;
-    }
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import 'swiper/swiper-bundle.min.css'
+import "swiper/components/scrollbar/scrollbar.min.css"
+
+// import "./styles.css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Scrollbar, Mousewheel
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Scrollbar,Mousewheel]);
+
+
+export default function CarrouselVideos() {
+  
+  
+  
+  return (
+    <>
+    <Swiper direction={'horizontal'} slidesPerView={'auto'} freeMode={true} scrollbar={true} mousewheel={true} className="mySwiper">
+    <SwiperSlide>
+    <h4>Scroll Container</h4>
+    <p>2</p>
+    <p>12</p>
+    <p>27</p>
+    <p>12</p>
+    <p>2</p>
+    <p>10 </p>
+    </SwiperSlide>
+  </Swiper>
+    </>
+  )
 }
-;
